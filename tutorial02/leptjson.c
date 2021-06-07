cc055165b17eb16746a9c58b444b746e9da5d295
soul <dh_souls@163.com>
2021-06-07 17:57:29 +0800

    test2.1 part

--- tutorial02/leptjson.c
+++ tutorial02/leptjson.c
@@ -1,6 +1,7 @@
 #include "leptjson.h"
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL, strtod() */
+#include <string.h>
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 
@@ -14,7 +15,7 @@ static void lept_parse_whitespace(lept_context* c) {
         p++;
     c->json = p;
 }
-
+#if 0
 static int lept_parse_true(lept_context* c, lept_value* v) {
     EXPECT(c, 't');
     if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
@@ -41,6 +42,30 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     v->type = LEPT_NULL;
     return LEPT_PARSE_OK;
 }
+#endif
+
+static int lept_parse_literal(lept_context* c, lept_value* v, const char* typeStr,lept_type type){
+    if(strcmp(c->json,typeStr)==0){
+        c->json += strlen(typeStr);
+        switch(*typeStr){
+            case 'f':
+                v->type=LEPT_FALSE;
+                break;
+            case 't':
+                v->type=LEPT_TRUE;
+                break;
+            case 'n':
+                v->type=LEPT_NULL;
+                break;
+            default:
+                v->type=LEPT_NULL;
+        }
+        return LEPT_PARSE_OK;
+    }
+    else{
+        return LEPT_PARSE_INVALID_VALUE;
+    }
+}
 
 static int lept_parse_number(lept_context* c, lept_value* v) {
     char* end;
@@ -55,9 +80,9 @@ static int lept_parse_number(lept_context* c, lept_value* v) {
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
-        case 't':  return lept_parse_true(c, v);
-        case 'f':  return lept_parse_false(c, v);
-        case 'n':  return lept_parse_null(c, v);
+        case 't':  return lept_parse_literal(c, v,"true",LEPT_TRUE);
+        case 'f':  return lept_parse_literal(c, v,"false",LEPT_FALSE);
+        case 'n':  return lept_parse_literal(c, v,"null",LEPT_NULL);
         default:   return lept_parse_number(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
     }
